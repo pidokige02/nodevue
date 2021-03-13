b3ccdbb08b37bdd2cda5e3e4f7721447bb05ee10
pidokige02 <43601998+pidokige02@users.noreply.github.com>
2021-03-13 20:35:09 +0900

    Create ogstest.js
    ogstest sample

---
+++ test/ogstest.js
@@ -0,0 +1,8 @@
+const util = require('util');       // node js 가 기본적으로 제공하는 것
+const utils = require('../utils');  // 새로 만든것.
+
+let url = "https://naver.com";
+
+utils.ogsinfo(url, (err, ret) => {
+  util.log(err, ret);
+});
