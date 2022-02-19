d56d8cddbfda6f228130a9ad3626665d74fe259a
pidokige02 <pidokige0204@gmail.com>
2022-02-19 23:56:43 +0900

    update @FW07 2022

--- vue/survey2/src/router/index.js
+++ vue/survey2/src/router/index.js
@@ -8,27 +8,27 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',                  // cf hash
   base: process.env.BASE_URL,
-  linkExactActiveClass: 'active',   //
+  linkExactActiveClass: 'active',   // 이 router 로가면 간 router 에 active 를 붙여 주는 것.
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
     },
-    { 
+    {
       path: '/survey/:id', name: 'survey', component: () => import('../views/Survey')
     },
-    { 
+    {
       path: '/adm', name: 'adm', component: () => import('../views/Adm')
-    }, 
-    { 
+    },
+    {
       path: '/surveylist', name: 'surveylist', component: () => import('../views/SurveyList'),
       children: [
-        { 
+        {
           path: '/surveylist/surveyedit/:id', name: 'surveyedit', component: () => import('../views/SurveyEdit')
-        },    
+        },
       ]
-    }, 
+    },
     {
       path: '/about',
       name: 'About',
@@ -37,6 +37,6 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
-    
+
   ]
 });
--- vue/survey2/src/views/SurveyEdit.vue
+++ vue/survey2/src/views/SurveyEdit.vue
@@ -1,16 +1,16 @@
 <template>
   <section>
-    
+
     <h5 class="card-header info-color white-text text-center py-3">
         <strong>{{survey.title}}</strong>
-        <a @click.prevent="togglesurveyEditing()" href="#" class="pull-left"><i class="fa fa-edit"></i></a>
+        <a @click.prevent="togglesurveyEditing()" href="#" class="pull-right"><i class="fa fa-edit"></i></a>
     </h5>
 
     <div v-if="isEditing" class="card-body pt-lg-5 pt-0">
       <form>
         <!-- Email input -->
         <div class="form-outline mb-4">
-          <input type="text" id="title" v-model="survey.title"/>
+          <input type="text" id="title" v-model="survey.title" class="form-control"/>
           <label for="title">설문 제목</label>
         </div>
 
@@ -23,16 +23,16 @@
         <!-- 2 column grid layout for inline styling -->
         <div class="row mb-4">
           <div class="col d-flex justify-content-center">
-            <button @click.prevent="togglesurveyEditing()" class="btn btn-outline-default btn-rounded btn-block my-4 waves-effect z-depth-0" 
+            <button @click.prevent="togglesurveyEditing()" class="btn btn-outline-default btn-rounded btn-block my-4 waves-effect z-depth-0"
             type="submit">취소</button>
-            <button class="ml-2 btn btn-outline-danger btn-rounded btn-block my-4 waves-effect z-depth-0" 
+            <button class="ml-2 btn btn-outline-danger btn-rounded btn-block my-4 waves-effect z-depth-0"
             type="submit">삭제</button>
-            <button class="ml-2 btn btn-outline-primary btn-rounded btn-block my-4 waves-effect z-depth-0" 
+            <button class="ml-2 btn btn-outline-primary btn-rounded btn-block my-4 waves-effect z-depth-0"
             type="submit">저장</button>
           </div>
         </div>
       </form>
-    </div>  
+    </div>
   </section>
 </template>
 
